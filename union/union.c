af38a294f791b9de79177d0633c48f438fd30a2c
mahrinaa <157700351+mahrinaa@users.noreply.github.com>
2024-11-26 23:10:17 +0100

    Add files via upload

--- union/union.c
+++ union/union.c
@@ -1,32 +1,48 @@
 #include <unistd.h>
 
+int	check_doublon(char *s, char c, int pos)
+{
+	int	i;
+
+	i = 0;
+	while (i < pos)
+	{
+		if (s[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_union(char *s1, char *s2)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (s1[i])
+	{
+		if (!check_doublon(s1, s1[i], i))
+		{
+			write(1, &s1[i], 1);
+		}
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		if (!check_doublon(s1, s2[j], i) && !check_doublon(s2, s2[j], j))
+		{
+			write(1, &s2[j], 1);
+		}
+		j++;
+	}
+}
+
 int	main(int ac, char **av)
 {
 	if (ac == 3)
-	{
-		unsigned char seen[256] = {0};
-		int i = 0;
-		int j = 0;
-
-		while (av[1][i])
-		{
-			if (!seen[(unsigned char)av[1][i]])
-			{
-				write(1, &av[1][i], 1);
-				seen[(unsigned char)av[1][i]] = 1;
-			}
-			i++;
-		}
-		while (av[2][j])
-		{
-			if (!seen[(unsigned char)av[2][j]])
-			{
-				write(1, &av[2][j], 1);
-				seen[(unsigned char)av[2][j]] = 1;
-			}
-			j++;
-		}
-	}
+		ft_union(av[1], av[2]);
 	write(1, "\n", 1);
 	return (0);
 }
