3d4e85d44a61055ae7ee4b93c2021639ccb09cda
mahrinaa <157700351+mahrinaa@users.noreply.github.com>
2024-12-13 03:54:17 +0100

    Update ft_list_size.c

--- ft_list_size/ft_list_size.c
+++ ft_list_size/ft_list_size.c
@@ -24,3 +24,72 @@ int	ft_list_size(t_list *begin_list)
 	}
 	return (size);
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "ft_list.h"
+
+// Fonction pour créer un nouveau nœud
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_node;
+
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+// Fonction pour libérer la mémoire d'une liste chaînée
+void	free_list(t_list *list)
+{
+	t_list	*temp;
+
+	while (list)
+	{
+		temp = list->next;
+		free(list);
+		list = temp;
+	}
+}
+/*
+int	main(void)
+{
+	t_list	*list;
+	t_list	*node1;
+	t_list	*node2;
+	t_list	*node3;
+	int		size;
+
+	// Création des nœuds
+	node1 = ft_lstnew("42 Network");
+	node2 = ft_lstnew("Piscine");
+	node3 = ft_lstnew("Cursus");
+
+	// Vérification des allocations
+	if (!node1 || !node2 || !node3)
+	{
+		free_list(node1);
+		free_list(node2);
+		free_list(node3);
+		printf("Erreur : Allocation échouée\n");
+		return (1);
+	}
+
+	// Chaînage des nœuds
+	list = node1;
+	node1->next = node2;
+	node2->next = node3;
+
+	// Calcul de la taille de la liste
+	size = ft_list_size(list);
+
+	// Affichage de la taille de la liste
+	printf("Taille de la liste : %d\n", size);
+
+	// Libération de la mémoire
+	free_list(list);
+	return (0);
+}*/
